404082dc84a216962f9302e419145806ca9e5922
lctking <1713673102lw@gmail.com>
2025-01-27 15:13:15 +0800

    init

---
+++ src/main/java/com/lctking/gitteamtest/GitTeamTestApplication.java
@@ -0,0 +1,13 @@
+package com.lctking.gitteamtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTeamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTeamTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lctking/gitteamtest/GitTeamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lctking.gitteamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTeamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
